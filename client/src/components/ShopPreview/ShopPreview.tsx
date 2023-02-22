d72e1550b9cec2a972b39bdea54027d37489d9ab
turza1994 <turza1994@yahoo.com>
2023-02-22 23:40:55 +0600

    fixing dark theme issues and adding spinner when loading products

--- client/src/components/ShopPreview/ShopPreview.tsx
+++ client/src/components/ShopPreview/ShopPreview.tsx
@@ -2,21 +2,39 @@ import { getCategories } from '@/api'
 import React, { useEffect, useState } from 'react'
 import CollectionPreview from '../CollectionPreview/CollectionPreview'
 
+const initialState = {
+  data: [],
+  isLoading: true,
+  error: '',
+}
+
 const ShopPreview = () => {
-  const [categories, setCategories] = useState<any>([])
+  const [categories, setCategories] = useState<any>(initialState)
+
   useEffect(() => {
     getCategories()
-      .then((response) => setCategories(response.data))
+      .then((response) =>
+        setCategories({ ...categories, data: response.data, isLoading: false })
+      )
       .catch((error) => console.log(error))
   }, [])
-  console.log(categories)
 
   return (
-    <div className=' flex flex-col gap-8'>
-      {categories.map((category: any) => (
-        <CollectionPreview key={category._id} category={category} />
-      ))}
-    </div>
+    <>
+      {categories.isLoading ? (
+        <div className='min-h-[50vh] flex justify-center items-center '>
+          <span className='animate-bounce text-primary text-3xl font-bold'>
+            Loading...
+          </span>
+        </div>
+      ) : (
+        <div className=' flex flex-col gap-8'>
+          {categories.data.map((category: any) => (
+            <CollectionPreview key={category._id} category={category} />
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
--- client/src/redux/productSlice.ts
+++ client/src/redux/productSlice.ts
@@ -1,42 +1,42 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import * as api from '../api/index'
 
-export interface ProductState {
-  isLoading: boolean
-  products:
-    | [
-        {
-          _id: string
-          title: string
-          message: string
-          creator: string
-          tags: [string]
-          selectedFile: string
-          likeCount: number
-          createdAt: string
-          __v: number
-        }
-      ]
-    | []
-  error: string | null
-}
+// export interface ProductState {
+//   isLoading: boolean
+//   products:
+//     | [
+//         {
+//           _id: string
+//           title: string
+//           message: string
+//           creator: string
+//           tags: [string]
+//           selectedFile: string
+//           likeCount: number
+//           createdAt: string
+//           __v: number
+//         }
+//       ]
+//     | []
+//   error: string | null
+// }
 
-export const getProducts = createAsyncThunk('product/getProducts', async () => {
-  let { data } = await api.getProducts()
-  data = data.slice(0, 4)
-  data = [...data, ...data]
-  //sort(() => Math.random() - 0.5)
-  return data
-})
+// export const getProducts = createAsyncThunk('product/getProducts', async () => {
+//   let { data } = await api.getProducts()
+//   data = data.slice(0, 4)
+//   data = [...data, ...data]
+//   //sort(() => Math.random() - 0.5)
+//   return data
+// })
 
-export const loadMoreProducts = createAsyncThunk(
-  'product/loadMoreProducts',
-  async () => {
-    let { data } = await api.getProducts()
-    data = data.slice(0, 4)
-    return data
-  }
-)
+// export const loadMoreProducts = createAsyncThunk(
+//   'product/loadMoreProducts',
+//   async () => {
+//     let { data } = await api.getProducts()
+//     data = data.slice(0, 4)
+//     return data
+//   }
+// )
 
 // export const addPost = createAsyncThunk(
 //   'contacts/addProduct',
@@ -58,28 +58,28 @@ const productSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      //Get All Products
-      .addCase(getProducts.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(getProducts.fulfilled, (state, action: any) => {
-        state.isLoading = false
-        state.products = action.payload
-      })
-      .addCase(getProducts.rejected, (state, action) => {
-        state.error = action.error.message
-      })
-      //Load More Products
-      .addCase(loadMoreProducts.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(loadMoreProducts.fulfilled, (state, action: any) => {
-        state.isLoading = false
-        state.products = [...state.products, ...action.payload]
-      })
-      .addCase(loadMoreProducts.rejected, (state, action) => {
-        state.error = action.error.message
-      })
+    //Get All Products
+    // .addCase(getProducts.pending, (state) => {
+    //   state.isLoading = true
+    // })
+    // .addCase(getProducts.fulfilled, (state, action: any) => {
+    //   state.isLoading = false
+    //   state.products = action.payload
+    // })
+    // .addCase(getProducts.rejected, (state, action) => {
+    //   state.error = action.error.message
+    // })
+    // //Load More Products
+    // .addCase(loadMoreProducts.pending, (state) => {
+    //   state.isLoading = true
+    // })
+    // .addCase(loadMoreProducts.fulfilled, (state, action: any) => {
+    //   state.isLoading = false
+    //   state.products = [...state.products, ...action.payload]
+    // })
+    // .addCase(loadMoreProducts.rejected, (state, action) => {
+    //   state.error = action.error.message
+    // })
 
     //Creating a post
     // .addCase(addPost.pending, (state) => {
